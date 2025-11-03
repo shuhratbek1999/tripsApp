7ad60431bfcaa6e39ecfdaa83b81cdc1090fc9e4
shuxratbek99 <shuhratbekzokirjonov1119@gmail.com>
2025-11-03 21:16:17 +0500

    finished tugadu

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
-// next.config.js
-const path = require("path");
+import type { NextConfig } from "next";
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.watchOptions = {
-        ignored: [
-          path.resolve("C:\\pagefile.sys"),
-          "**/node_modules/**",
-          "**/.next/**",
-          "**/out/**",
-        ],
-      };
-    }
-
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
+const nextConfig: NextConfig = {
+  /* config options here */
 };
 
-module.exports = nextConfig;
+export default nextConfig;
